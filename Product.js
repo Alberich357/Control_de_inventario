bfd21f79fc4a53f7cea4dcb982e85e525d008d55
Alberich357 <allerenas4@ucol.mx>
2021-09-19 23:40:51 -0500

    html

--- Product.js
+++ Product.js
@@ -1,3 +1,6 @@
+//Tarea incompleta, se me juntaron varias cosas y no pude iniciar hasta el sabado, recien ahorita cuando andaba terminando 
+//vi que usar el static read form esta mal asi que mejor dejo lo que creo que pueda estar bien para cuando sepa como hacerlo
+//sin el read form
 export default class Product{
     constructor(id,name, quantity, cost){
         this._id = id;

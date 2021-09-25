d7c7161abd4d42320a6dc954d3a06d4d9d6f0a93
Alberich357 <allerenas4@ucol.mx>
2021-09-25 00:56:08 -0500

    Main constructor and readform fixed

--- main.js
+++ main.js
@@ -2,10 +2,39 @@ import Product from "./Product.js";
 import Registry from "./registry.js";
 class App {
     constructor() {
-        this._registry = new Registry();
-        let btnRegister = document.querySelector("#btnRegister");
-
-    btnRegister.addEventListener("click", this._addParticipant);
+        this._inventory= new Inventory();
+            const btnRegister= document.getElementById("btnAdd");
+            const  btnSearch=document.getElementById("btnSearch");
+            const  btnBackwardlist=document.getElementById("btnBackwardlist");
+            const  btnInsert=document.getElementById("btnInsert");
+            const  btnDelete=document.getElementById("btnDelete");
+            const  btnList=document.getElementById("btnList");
+          btnInsert.addEventListener("click",  this._insertProduct)
+          btnBackwardlist.addEventListener("click",  this.BackwardsList)
+          btnDelete.addEventListener("click",  this._deleteProduct)
+          btnList.addEventListener("click",  this._listProducts)
+          btnSearch.addEventListener("click",  this._searchProduct)
+        btnRegister.addEventListener("click", this._addProduct)    
+      }
+      readForm(){
+        let inpId=document.getElementById("id");
+        let inpName=document.getElementById("name");
+        let inpQuantity=document.getElementById("quantity");
+        let inpCost=document.getElementById("cost");
+        let id=inpId.value;
+        let name=inpName.value;
+        let quantity=inpQuantity.value
+        let cost=inpCost.value
+        if(id&&name&&quantity&&cost){
+            inpId.value="";
+            inpName.value="";
+            inpQuantity.value="";
+            inpCost.value=""; 
+            id=Number(id)
+            quantity=Number(quantity)
+            cost=Number(cost)
+            return new Product(id, name, quantity, cost);
+        }
     }
 }
 new App;
